ac75d91a553354a76fb29956e7eb0a4219be260d
chicorasia <chicorasia@gmail.com>
2021-08-12 17:32:41 -0300

    Layout do AddCardFragment modificado, adicionados chips

--- app/src/main/java/br/com/chicorialabs/businesscard/ui/addcard/AddCardFragment.kt
+++ app/src/main/java/br/com/chicorialabs/businesscard/ui/addcard/AddCardFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import br.com.chicorialabs.businesscard.R
 import br.com.chicorialabs.businesscard.databinding.FragmentAddCardBinding
 
 
@@ -34,8 +36,9 @@ class AddCardFragment : Fragment() {
          */
         binding.navController = findNavController()
         binding.lifecycleOwner = viewLifecycleOwner
+
         return binding.root
-    }
+     }
 
     companion object {
 
