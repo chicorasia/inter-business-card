0c694673c129f2efb8afc51534bc194d7b31bac8
chicorasia <chicorasia@gmail.com>
2021-08-12 14:22:09 -0300

    Criado layout basico do AddCardFragment

--- app/src/main/java/br/com/chicorialabs/businesscard/data/BusinessCard.kt
+++ app/src/main/java/br/com/chicorialabs/businesscard/data/BusinessCard.kt
@@ -4,6 +4,10 @@ package br.com.chicorialabs.businesscard.data
  * Essa data class representa a única entidade usada no app. Aqui foi implementada minimamente só
  * para iniciar a configuração do ViewModel.
  */
+
+// TODO: Adicionar os outros atributos da classe
+// TODO: Transformar em uma @Entity para o Room
+
 data class BusinessCard(val nome: String) {
 
 }
